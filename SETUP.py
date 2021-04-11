fd3e9569de84f1f19cf52fe183882bc0f2bd6498
Error 999: No_Name <aditya.funs.11@gmail.com>
2021-04-11 07:38:40 +0000

    Update SETUP.py

--- SETUP.py
+++ SETUP.py
@@ -1,11 +1,19 @@
 import os
+import time
+from datetime import datetime
+
+now = datetime.now()
+date = now.strftime("%d/%m/%y @ %H:%M")
 
 def osCheck():
     ck = os.name
     if ck == "posix":
-        main()
+        print("{+} Linux OS Found, Script can continue...")
+        selenium()
+        
     elif ck == "nt":
         print("{!} Windows OS found, Script can't continue")
+        print("{+} Support for Windows will be available in future updates")
         quit()
     elif ck == "osx":
         print("{!} OSX found, Script can't continue")
@@ -18,23 +26,42 @@ def osCheck():
 def selenium():
     try:
         from selenium import webdriver
-    except Exception as e:
+    except ImportError as e:
         # print(e)
+        f = open("ErrorLogs.log","a")
+        f.write("["+str(date)+"] >> {SETUP} "+str(e)+".\n")
+        print("")
         print("{!} Selenium is not installed")
         ask = input("{?} Do you want to install Selenium Now \n    [Y/N]: ")
         if ask == "Y"or"y":
             print("{*} Installing Selenium...")
-            os.system("pip install selenium")
+            os.system("xterm pip install selenium")
             main()
         elif ask == "N"or"n":
-            print("{!} LocatoteMe needs Selenium")
+            print("{!} LocateMe loves Selenium, LocateMe cannot work without it!!")
             print("{*} Exiting...")
             quit()
          else:
             print("{!} Wrong Option Chosen")
             return ask
+    except ConnectionError as ce:
+        #print(ce)
+        print("{!} No Internet Connection")
+        print("{*} Exiting...")
+        time.sleep(2)
+        quit()
         
-def lolcate:
+def main():
+    os.system("apt-get install lolcat figlet xterm && pip install selenium")
+    ASK = ("{?} START LocateMe [Y/N]: ")
+    if ASK == "Y"or"y":
+        os.system("python3 LocateMe.py")
+        quit()
+    else:
+        print("{*} Exiting...")
+        time.sleep(2)
+        quit()
     
-def figlet:
-   
+    
+if __name__ == "__main__":
+    osCheck()
